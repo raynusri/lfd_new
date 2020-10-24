a81ea8104cdeb90ff208368003c333445a1c3780
Hawwa11 <aliyanajackson39@gmail.com>
2020-10-25 02:02:08 +0800

    import statement

--- app/src/main/java/com/example/lfd/Register.java
+++ app/src/main/java/com/example/lfd/Register.java
@@ -9,7 +9,8 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import java.util.HashMap;
+import java.util.Map;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
