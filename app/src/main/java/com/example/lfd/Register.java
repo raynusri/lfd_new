15c85dd310d910a881ac64ffb35939a04febff49
Hawwa11 <aliyanajackson39@gmail.com>
2020-10-25 05:01:37 +0800

    firestore(25/10)

--- app/src/main/java/com/example/lfd/Register.java
+++ app/src/main/java/com/example/lfd/Register.java
@@ -3,8 +3,10 @@ package com.example.lfd;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -15,20 +17,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Register extends AppCompatActivity {
-    public static final String TAG = "TAG";
+    public static final String TAG = "Register";
     EditText mFullName,mEmail,mPassword,mPhone,mdob;
     Button mRegisterBtn;
     TextView mLoginBtn,policy;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
     String userID;
-
+CheckBox agree;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +50,8 @@ public class Register extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.CreateText);
 policy=findViewById(R.id.policy);
         fAuth = FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+        agree =findViewById(R.id.checkBox);
 
         progressBar = findViewById(R.id.progressBar);
 
@@ -55,8 +63,11 @@ finish();
 
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                final String fullname= mFullName.getText().toString();
+                final String phone =mPhone.getText().toString();
+                final String dob =mdob.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is required.");
@@ -70,6 +81,11 @@ finish();
                     mPassword.setError("password must be more than 6 characters");
                     return;
                 }
+
+                if(agree.isChecked()==false){
+                    agree.setError("Please agree to term and conditions.");
+                    return;
+                }
                 progressBar.setVisibility(View.VISIBLE);
 
 
@@ -79,7 +95,20 @@ finish();
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            userID=fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fStore.collection("users").document(userID);
+Map<String,Object> user=new HashMap<>();
+user.put("fName",fullname);
+user.put("email",email);
+user.put("phone",phone);
+user.put("dob",dob);
+documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+    @Override
+    public void onSuccess(Void aVoid) {
+Log.d(TAG,"OnSuccess:user Profile Created "+userID);
+    }
+});
+startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
